70f036b370fcf094cbe31324ddb06416e83bb06b
Tin9oo <rlawnghks99@gmail.com>
2024-04-02 17:41:59 +0900

    feat: 이미지 변환 코드 작성
    Byte 데이터를 String으로 변환 후 다시 Byte 데이터로 변환하면서 데이터 손실이 발생하는지 확인하기 위한 코드 작성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,73 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.*;
+import java.util.Arrays;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static String url = "src/main/resources/A84F44A7-0D89-4E37-B686-5B4991422C05_1_105_c.jpeg";
+    static File file;
+
+    static {
+        file = new File(url);
+    }
+
+    // 파일을 한번에 읽는 코드
+    private static byte[] convertFile() {
+        try (InputStream inputStream = new FileInputStream(file);
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream)) {
+
+            byte[] buffer = new byte[(int) file.length()];
+            bufferedInputStream.read(buffer);
+            return buffer;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    // 라인 단위로 읽는 코드
+    private static byte[] convertLine() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append(System.lineSeparator());
+            }
+            content.append("\r\n");
+            return content.toString().getBytes();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // 바이트 단위로 읽는 코드
+    private static byte[] convertByte() {
+        try (InputStream inputStream = new FileInputStream(file)) {
+            // 파일 내용을 바이트 배열로 읽어오기
+            byte[] buffer = new byte[(int) file.length()];
+            int bytesRead = inputStream.read(buffer);
+
+            // bytesRead가 -1이 아니면 계속 읽기
+            while (bytesRead != -1) {
+                bytesRead = inputStream.read(buffer);
+            }
+
+            return buffer;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+        System.out.println();
+
+        System.out.println(Arrays.toString(convertFile()));
+        System.out.println(Arrays.toString(convertLine()));
+        System.out.println(Arrays.toString(convertByte()));
     }
 }
